ca7c8b23af82f1dd1094ccd21e3cc4e55693e344
Artak Sargsyan <106394723+sartak812@users.noreply.github.com>
2025-06-19 23:37:27 +0400

    Add files via upload

---
+++ Get-NewADComputersByMonth.ps1
@@ -0,0 +1,9 @@
+﻿$startOfFebruary = Get-Date -Year (Get-Date).Year -Month 5 -Day 1
+$endOfFebruary = $startOfFebruary.AddMonths(1).AddDays(-1)
+
+Get-ADComputer -Filter * -Property OperatingSystem, whenCreated | Where-Object {
+    ($_.whenCreated -ne $null) -and (
+        $_.whenCreated -ge $startOfFebruary -and
+        $_.whenCreated -le $endOfFebruary
+    )
+} | Select-Object Name, OperatingSystem, whenCreated
